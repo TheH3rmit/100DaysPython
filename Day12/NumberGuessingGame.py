009dc32b45be6497ac40a3f8a4fdd9ee59724c7d
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-13 21:41:50 +0200

    Day12 limited chances/lives feature
    Introduction of difficulty level and limited chances to guess correct number

--- Day12/NumberGuessingGame.py
+++ Day12/NumberGuessingGame.py
@@ -15,13 +15,28 @@ def validate_input(new_int: str) -> int:
         return new_int
 
 
+def set_lives_difficulty(difficulty: str) -> int:
+    match difficulty:
+        case "hard":
+            guesses_limit = 10
+            return guesses_limit
+        case "normal":
+            guesses_limit = 20
+            return guesses_limit
+        case "easy":
+            guesses_limit = 30
+            return guesses_limit
 
 
+def lose_condition_check(number_of_guesses) -> bool:
+    if number_of_guesses == 0:
+        print("You lost")
+        return True
+    return False
 
-answer = random.randint(0, 100)
 
-
-while True:
+def main_game_loop():
+    print(f"You have {lives} chances to guess correct number")
     print("Choose a number between 0 and 100 including")
     user_guess = input()
     user_guess = validate_input(user_guess)
@@ -31,3 +46,12 @@ while True:
         print(f"The number that I'm thinking about is larger than {user_guess}")
     elif answer < user_guess:
         print(f"The number that I'm thinking about is smaller than {user_guess}")
+
+
+while True:
+    answer = random.randint(0, 100)
+    print("Write 'Hard', 'Normal', 'Easy' to choose difficulty level of the game'")
+    difficulty = input().lower()
+    lives = set_lives_difficulty(difficulty)
+    while not lose_condition_check(lives):
+        main_game_loop()
