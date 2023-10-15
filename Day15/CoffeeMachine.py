8309da445a1d36700e2bcdeebae93576d0bab010
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-16 01:57:36 +0200

    Day 15 additional features

--- Day15/CoffeeMachine.py
+++ Day15/CoffeeMachine.py
@@ -1,3 +1,5 @@
+import time
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -25,44 +27,95 @@ MENU = {
 }
 
 resources = {
-    'water': 300,
-    'milk': 200,
-    'coffee': 100,
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
 }
 
-def resources_raport():
+
+def resources_report():
     print(
         f"Water:{resources['water']}ml\n"
         f"Milk:{resources['milk']}ml\n"
         f"Coffee:{resources['coffee']}g\n"
-        f"Money:{resources['money']}$\n"
+        f"Money:{profit}$\n"
     )
 
-    
+
+def handle_input() -> str:
+    print("What would you like?")
+    print("Espresso, Latte, Cappuccino")
+    return input().lower()
+
+
+def handle_choice(chosen: str):
+    match chosen:
+        case "espresso":
+            print("Espresso case")
+            return chosen
+        case "latte":
+            print("Latte case")
+            return chosen
+        case "cappuccino":
+            print("Cappuccino case")
+            return chosen
+        case "report":
+            print("report case")
+            resources_report()
+        case "off":
+            print("Off case")
+            for n in range(5):
+                print(f"Machine shutting down in {5-n} seconds")
+                time.sleep(1)
+            exit(0)
+        case _:
+            print("default case")
+
+
+def resources_check(chosen_product: str, resources_list: dict, menu: dict) -> bool:
+    for i in menu[chosen_product]["ingredients"]:
+        difference = resources_list[i] - menu[chosen_product]["ingredients"][i]
+        if difference < 0:
+            print(f"Sorry there is not enough {i}.")
+            return False
+        else:
+            return True
+
+
+def make_coffee(chosen_product: str, resources_list: dict, menu: dict) -> str:
+    for i in menu[chosen_product]["ingredients"]:
+        resources_list[i] -= menu[chosen_product]["ingredients"][i]
+    return f"Here is your {chosen_product}. Enjoy!"
+
+
+def handle_coins() -> float:
+    print("Insert coins")
+    credit = float(input())
+    return credit
+
+
+def purchase_check(inserted_coins, chosen_product: str, menu: dict) -> bool:
+    global profit
+    if inserted_coins >= menu[chosen_product]["cost"]:
+        difference = inserted_coins - menu[chosen_product]["cost"]
+        profit += menu[chosen_product]["cost"]
+        if difference > 0:
+            print(f"Returning {difference}")
+        return True
+    else:
+        print(f"Sorry that's not enough money. {inserted_coins} refunded.")
+        return False
+
+
+profit = 0
 coffee_machine_active = True
-
+order_in_progress = False
 while coffee_machine_active:
-    break
-print("What would you like?")
-print("Espresso, Latte, Cappuccino")
-choice = input().lower()
-
-
-match choice:
-    case "espresso":
-        print("Espresso case")
-    case "latte":
-        print("Latte case")
-    case "cappuccino":
-        print("Cappuccino case")
-    case "off":
-        print("Off case")
-    case _:
-        print("default case")
-
-
-resources_raport()
-
-
-purchase_successful = True
-enough_resources = True
+    if not order_in_progress:
+        choice = handle_input()
+        handle_coins(choice)
+        enough_resources = resources_check(handle_choice(choice), resources, MENU)
+        successful_purchase = purchase_check(handle_coins(), handle_choice(choice), MENU)
+        if enough_resources and successful_purchase:
+            make_coffee(choice, resources, MENU)
+            order_in_progress = False
