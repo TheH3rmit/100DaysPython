563464db5bcdb776b2ff79314638681c0558aa09
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-18 22:41:38 +0200

    Day 16 finished

--- Day16/coffeeMachineOOP.py
+++ Day16/coffeeMachineOOP.py
@@ -1,3 +1,38 @@
 from menu import Menu, MenuItem
 from coffeeMaker import CoffeeMaker
 from moneyMachine import MoneyMachine
+
+menu = Menu()
+coffeeMachine = CoffeeMaker()
+moneyMachine = MoneyMachine()
+
+
+def handle_input() -> str:
+    print("What would you like?")
+    print(menu.get_items())
+    return input().lower()
+
+
+while True:
+    user_input = handle_input()
+    selected_drink = MenuItem
+    if user_input == "report":
+        coffeeMachine.report()
+        moneyMachine.report()
+
+    for item in menu.menu:
+        if user_input == item.name:
+            selected_drink = item
+            break
+    else:
+        print("Item not found in the menu.")
+        continue
+
+    if coffeeMachine.is_resource_sufficient(selected_drink):
+        if moneyMachine.make_payment(selected_drink.cost):
+            coffeeMachine.make_coffee(selected_drink)
+        else:
+            continue
+    else:
+        print("Not enough resources")
+        continue
