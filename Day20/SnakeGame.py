4e0710a59de5b9581bbd3df13e6b07b3c8c0c0ec
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-23 23:37:01 +0200

    Snake movment

--- Day20/SnakeGame.py
+++ Day20/SnakeGame.py
@@ -1,9 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
 
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
@@ -14,9 +16,17 @@ for x in starting_positions:
     part.color("white")
     part.penup()
     part.setpos(x)
+    snake_parts.append(part)
+
+screen.update()
 
 while True:
-    for i in snake_parts:
-        i.fd(20)
+    screen.update()
+    time.sleep(0.1)
+    for i in range(len(snake_parts)-1,0,-1):
+        new_x = snake_parts[i-1].xcor()
+        new_y = snake_parts[i-1].ycor()
+        snake_parts[i].goto(new_x,new_y)
+    snake_parts[0].fd(20)
 
 screen.exitonclick()
