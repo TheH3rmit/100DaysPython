2855093e1f62ee57be842b6aae9027e7806a0d65
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-28 18:27:11 +0200

    Formating update

--- Day20/food.py
+++ Day20/food.py
@@ -1,17 +1,18 @@
 import random
-import turtle
 from turtle import Turtle
+
+
 class Food(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("red")
         self.speed("fastest")
         self.food_eaten()
 
     def food_eaten(self):
-        x_random = random.randint(-270,270)
+        x_random = random.randint(-270, 270)
         y_random = random.randint(-270, 270)
-        self.goto(x_random,y_random)
+        self.goto(x_random, y_random)
