f0653628d765ac04bd5c1c4218261b73b67b3f71
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-28 17:27:51 +0200

    Fix for issue of food spawning at the (0,0)

--- Day20/food.py
+++ Day20/food.py
@@ -9,6 +9,7 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5,stretch_wid=0.5)
         self.color("red")
         self.speed("fastest")
+        self.food_eaten()
 
     def food_eaten(self):
         x_random = random.randint(-270,270)
