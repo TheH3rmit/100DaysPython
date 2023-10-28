e300784345245a15dbc1c1a04ea1061a40d2c150
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2023-10-28 18:45:04 +0200

    Writing game over

--- Day20/scoreboard.py
+++ Day20/scoreboard.py
@@ -9,8 +9,9 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score_number = 0
-        self.static_text = "Score: " + str(self.score_number)
-        self.write(self.static_text, False, align="center", font=('Arial', 16, 'normal'))
+        self.game_over_text = "GAME OVER"
+        self.score_text = "Score: " + str(self.score_number)
+        self.write(self.score_text, False, align="center", font=('Arial', 16, 'normal'))
 
     def add_point(self):
         self.score_number += 1
@@ -18,5 +19,9 @@ class Scoreboard(Turtle):
 
     def show_score(self):
         self.clear()
-        self.static_text = "Score: " + str(self.score_number)
-        self.write(self.static_text, False, align="center", font=('Arial', 16, 'normal'))
+        self.score_text = "Score: " + str(self.score_number)
+        self.write(self.score_text, False, align="center", font=('Arial', 16, 'normal'))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(self.game_over_text, False, align="center", font=('Arial', 16, 'normal'))
