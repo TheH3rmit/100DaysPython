0570b9420b136b933119a912a3fdef58d37fd1a9
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2024-03-22 17:16:02 +0100

    Requirements for Pong Game

---
+++ Day22/PongGame.py
@@ -0,0 +1,29 @@
+#Display
+    # score
+    # player paddle
+    # opponnet paddle
+    # main playing field
+
+#Classes
+    #Ball
+    #Paddle
+    #Score Handler
+        #Should track score for both players or player and AI
+    #Game Manager
+        #Should handle the game flow from start to finish and restart
+        #Decides who wins after criteria (time, points)
+
+
+#Input
+    #player 1 and 2 input
+    #AI input
+
+#Game loop
+    #Game is launched
+    #Proceeds to the mainboard
+    #Select screen for AI or 2 players
+    #Select victory criteria both or one and input amount of time or amount of points or both
+    #Game starts
+    #Players or Player and AI play
+    #Victory criteria is met (time, points)
+    #Restart or quit option visible
